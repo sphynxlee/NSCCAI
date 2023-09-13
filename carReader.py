5b39ba33504812085c334ccbc83c5f7757a20f76
haidun.lhd <kaiton.ri@gmail.com>
2023-09-13 13:28:45 -0300

    BugID: 20230913: NN and Prg assignment

---
+++ carReader.py
@@ -0,0 +1,11 @@
+# print pwd
+# import os
+# pwd = os.getcwd() + '\NSCCAI\cars.csv'
+# print(pwd);
+
+# open the cars.csv file
+file = open('cars.csv', 'r');
+print(file.read());
+# for line in file:
+#     print(line);
+# file.close();
