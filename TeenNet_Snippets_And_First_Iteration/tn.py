a479df7fd7112ecffa49aa98e2e2a850f5b86c49
haidun.lhd <kaiton.ri@gmail.com>
2023-09-25 10:03:25 -0300

    BugID: 20230925: NN: update teenNet, babyNet, test.ipynb

--- TeenNet_Snippets_And_First_Iteration/tn.py
+++ TeenNet_Snippets_And_First_Iteration/tn.py
@@ -34,17 +34,17 @@ class TeenNet:
     # notice that our subtraction here is acting on numpy arrays
     # this is our - very simple - COST FUNCTION. It's living here
     # for now - because it's so simple.
-    output_errors = targets-final_out
+    output_errors = targets - final_out
 
 
     # use output errors to update weights between hidden layer and output layer
-    self.second_weights += self.learning_rate * numpy.matmul((output_errors * final_out  * (1.0-final_out)),numpy.transpose(hidden_out))
+    self.second_weights += self.learning_rate * numpy.matmul((output_errors * final_out  * (1.0 - final_out)),numpy.transpose(hidden_out))
 
 
     # backprop errors from output_errors to hidden_errors
     hidden_errors = numpy.matmul(self.second_weights.T,output_errors)
     # use hidden errors to update weights between input layer and hidden layer
-    self.first_weights += self.learning_rate * numpy.matmul((hidden_errors * hidden_out * (1.0-hidden_out)),numpy.transpose(inputs))
+    self.first_weights += self.learning_rate * numpy.matmul((hidden_errors * hidden_out * (1.0 - hidden_out)),numpy.transpose(inputs))
 
 
   def forward_pass(self,inputs_list):
