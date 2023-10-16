1e937d4654facfb4bcaf1e2b87ce207b857d3a12
haidun.lhd <kaiton.ri@gmail.com>
2023-10-16 12:22:37 -0300

    BugID: 20231016: NN: update pyteen, add predict

--- pyteen.py
+++ pyteen.py
@@ -1,7 +1,9 @@
+import torch
 from torch import nn, optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # training_data = datasets.MNIST(root="data",train=True, download=True, transform=None)
 # testing_data = datasets.MNIST(root="data",train=False, download=True, transform=None)
@@ -34,6 +36,11 @@ class PyTeen (nn.Module):
     def forward(self, input):
         return self.layers(input)
 
+    def predict(self, input):
+        with torch.no_grad():
+            pred = self.forward(input)
+            return (torch.argmax(pred, axis=-1))
+
     # train is a loss
     # label is a actual value
     def train(self, input, label):
@@ -61,7 +68,7 @@ pyTeen = PyTeen()
 EPOCH = 1
 for i in range(EPOCH):
     k, total_loss = 0, 0
-    for input, label in training_data_loader:
+    for input, label in tqdm(training_data_loader):
         # print(f'input: {input}\n')
         # print(f'label: {label}\n')
         total_loss += pyTeen.train(input, label)
@@ -71,3 +78,25 @@ for i in range(EPOCH):
     # print(f'total_loss for EPOCH {i} is: {total_loss/len(training_data_loader)}\n')
 
 
+
+# print(len(testing_data))
+# print(testing_data[0][0].shape)
+# print(len(testing_data_loader))
+# print(len(testing_data_loader.dataset))
+# print(testing_data_loader.dataset[0][0].shape)
+
+#  evaluation loop / testing loop
+# we want to call predict repeatedly with the testing data inputs
+k, total_correct = 0, 0
+for input, label in tqdm(testing_data_loader):
+    # print(f'input: {input}\n')
+    # print(f'label: {label}\n')
+    prediction = pyTeen.predict(input)
+    # print(f'prediction: {prediction}\n')
+    total_correct += (prediction == label).sum()
+    k += 1
+
+print(f'k is: {k}\n')
+print(f'total_correct: {total_correct}\n')
+# print(total_correct/len(testing_data_loader)*BATCH_SIZE)
+print((total_correct/len(testing_data_loader)*BATCH_SIZE).item())
