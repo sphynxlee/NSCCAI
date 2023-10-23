96ad764a7ecc167e8804daa2bb867b37fdff7048
haidun.lhd <kaiton.ri@gmail.com>
2023-10-23 09:33:40 -0300

    update client practice

--- cs/client.py
+++ cs/client.py
@@ -14,9 +14,14 @@ class Client:
             exit(1)
         print(f'response.headers content-type: {response.headers["content-type"]}')
         print(f'response: {response.text}')
+        self.response = response
 
-my_client = Client('http://127.0.0.1:5000/', '/inputValue/', 'hello')
-my_client.get()
+# my_client = Client('http://127.0.0.1:5000/', '/inputValue/', 'hello')
+# my_client.get()
 
-my_client2 = Client('http://127.0.0.1:5000/', '/calc/', '5,6')
-my_client2.get()
+# my_client2 = Client('http://127.0.0.1:5000/', '/calc/', '5,6')
+# my_client2.get()
+
+my_client3 = Client('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', '', '')
+my_client3.get()
+print(f'my_client3.response.json(): {my_client3.response.json()}')
