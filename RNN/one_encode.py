f7309f2eb5c979ccf6502d722ddb42e5cc043280
haidun.lhd <kaiton.ri@gmail.com>
2023-11-20 11:35:12 -0400

    update one hot encoding

--- RNN/one_encode.py
+++ RNN/one_encode.py
@@ -1,23 +1,18 @@
-words = ['I', 'like', 'apples', 'He', 'likes', 'python', 'She', 'is', 'learning', 'NLP']
-
-unique_words = set(words)
-# print(unique_words)
-
-word2int = dict(enumerate(unique_words))
-# print(word2int.values())
-# print(word2int.keys())
-print(f'word2int is {word2int}')
-
-int2word = dict(zip(word2int.values(), word2int.keys()))
-print(f'int2word is {int2word}')
-
-# number change to vector
-
 import numpy as np
 
-def one_hot_encoder(word, word2int):
-    # word_vec = [0] * len(word2int)
-    word_vec = np.zeros(len(word2int))
-    word_index = word2int[word]
-    word_vec[word_index] = 1
-    return word_vec
+def one_hot_encoder(words):
+    unique_words = list(set(words))
+    encoding_map = {word: i for i, word in enumerate(unique_words)}
+
+    # output encoding map
+    for word, i in encoding_map.items():
+        print(f'slot {i+1}: {word}')
+
+    # one hot encoding
+    one_hot_vectors = np.zeros((len(words), len(unique_words)), dtype=np.int32)
+    for i, word in enumerate(words):
+        one_hot_vectors[i, encoding_map[word]] = 1
+    return one_hot_vectors
+
+cars = ['BMW', 'Benz', 'Benz', 'Tesla', 'Toyota']
+print(one_hot_encoder(cars))
