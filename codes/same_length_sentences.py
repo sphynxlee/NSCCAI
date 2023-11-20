274b7c5732b0f3d016301cade795590bb11730f5
haidun.lhd <kaiton.ri@gmail.com>
2023-11-20 09:06:30 -0400

    code practice: same lengthe sentences

---
+++ codes/same_length_sentences.py
@@ -0,0 +1,21 @@
+def sentences_alignment (sentence_lists):
+    len_lists = []
+
+    for i in range(len(sentence_lists)):
+        len_lists.append(len(sentence_lists[i]))
+
+    max_len = max(len_lists)
+
+    for i in range(len(sentence_lists)):
+        if len_lists[i] < max_len:
+            for j in range(max_len - len_lists[i]):
+                sentence_lists[i] += ' '
+
+    return sentence_lists
+
+sentence_lists = [
+    'I like apples',
+    'He love python',
+    'She is learning NLP'
+]
+print(sentences_alignment(sentence_lists))
