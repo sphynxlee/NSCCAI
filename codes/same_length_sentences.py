c6a95016877496ec3154744ffed87746090b1e68
haidun.lhd <kaiton.ri@gmail.com>
2023-11-20 09:47:49 -0400

    more practice with string

--- codes/same_length_sentences.py
+++ codes/same_length_sentences.py
@@ -1,3 +1,4 @@
+# No.1
 def sentences_alignment (sentence_lists):
     len_lists = []
 
@@ -19,3 +20,52 @@ sentence_lists = [
     'She is learning NLP'
 ]
 print(sentences_alignment(sentence_lists))
+
+# No.2
+sentence_lists = [
+    'i like apples',
+    'He likes python',
+    'She is learning NLP'
+]
+
+def unique_words (sentence_lists):
+    result_sentences = []
+    for i in range(len(sentence_lists)):
+        result_sentences.append(set(sentence_lists[i]))
+
+    return result_sentences
+
+print(unique_words(sentence_lists))
+
+# No.3
+def get_unique_chrs(sentences):
+    chrs = set(''.join(sentences))
+    return (sorted(chrs))
+
+sentences = ['I like apples', 'He likes python', 'She is learning NLP']
+print(get_unique_chrs(sentences))
+
+# No.4
+letters = [' ', 'H', 'I', 'L', 'N', 'P', 'S', 'a', 'e', 'g', 'h', 'i', 'k', 'l', 'n', 'o', 'p', 'r', 's', 't', 'y']
+
+def get_int_map(vocab):
+    int2chr = dict(enumerate(vocab))
+    return (int2chr)
+print(get_int_map(letters))
+
+letters = [' ', 'H', 'I', 'L', 'N', 'P', 'S', 'a', 'e', 'g', 'h', 'i', 'k', 'l', 'n', 'o', 'p', 'r', 's', 't', 'y']
+def sorted_letters(letters):
+    result = {}
+    for i in range(len(letters)):
+        result[i] = letters[i]
+    return result
+print(sorted_letters(letters))
+
+# No.5
+obj = {0: ' ', 1: 'H', 2: 'I', 3: 'L', 4: 'N', 5: 'P', 6: 'S', 7: 'a', 8: 'e', 9: 'g', 10: 'h', 11: 'i', 12: 'k', 13: 'l', 14: 'n', 15: 'o', 16: 'p', 17: 'r', 18: 's', 19: 't', 20: 'y'}
+def reverse_key_value (obj):
+    result = {}
+    for key, value in obj.items():
+        result[value] = key
+    return result
+print(reverse_key_value(obj))
