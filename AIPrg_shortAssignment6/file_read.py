c30b9dceb7f881e1fca2142514ea71cd017be830
haidun.lhd <kaiton.ri@gmail.com>
2023-12-11 10:09:08 -0400

    update file read

--- AIPrg_shortAssignment6/file_read.py
+++ AIPrg_shortAssignment6/file_read.py
@@ -1,23 +1,30 @@
 import os
 
-pwd = os.getcwd() + '/NSCCAI/AIPrg_shortAssignment6'
+pwd = os.getcwd()
+pwd = pwd + '/NSCCAI/AIPrg_shortAssignment6'
+print(f'pwd is: {pwd}')
 
-source_file = pwd + '/source_file.txt'
-prohibited_syntax_file = pwd + '/prohibited_syntax.txt'
+source_file_path = os.path.join(pwd, 'source_file.txt')
+prohibited_syntax_file_path = os.path.join(pwd, 'prohibited_syntax.txt')
 
-with open(source_file, 'r') as f:
-    source = f.read().splitlines()
-    print(f'source_file content is: {source}')
+with open(source_file_path, 'r') as f:
+    source_file = f.read().splitlines()
+    print(f'source_file content is: {source_file}')
 
 print('\n')
 
-with open(prohibited_syntax_file, 'r') as f:
+with open(prohibited_syntax_file_path, 'r') as f:
     prohibited_syntax = f.read().splitlines()
     print(f'prohibited_syntax_file content is: {prohibited_syntax}')
 
+print(type(source_file), type(prohibited_syntax), '\n')
 
-print(type(source), type(prohibited_syntax), '\n')
+source_file_res = []
+for i in range(len(source_file)):
+    ele = source_file[i]
+    for j in range(len(prohibited_syntax)):
+        sub_ele = prohibited_syntax[j]
+        if sub_ele.lower() in ele.lower():
+            source_file_res.append(ele.replace(sub_ele, '***'))
 
-for i in range(len(prohibited_syntax)):
-    prohibited_syntax[i] = prohibited_syntax[i].split(' ')
-    print(f'line {i+1} is: {prohibited_syntax[i]}')
+print(f'new source_file_res content is: {source_file_res}')
